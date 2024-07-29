eef56d573138096b6f7f392c55191b60f4986401
duduumendez2 <169172180+duduumendez2@users.noreply.github.com>
2024-07-29 12:26:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -83,7 +83,7 @@ module.exports = {
   KOYEB_API : process.env.KOYEB_API  || "false",
 
   REMOVE_BG_KEY : process.env.REMOVE_BG_KEY  || "",
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "sk-proj-G3CjG2FT5pD7B8MMpuz4T3BlbkFJmIaLQ1jRbmXhNb826eCF",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
   HEROKU_APP_NAME:process.env.HEROKU_APP_NAME|| "",
   antilink_values:process.env.ANTILINK_VALUES|| "all",
