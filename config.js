6dbd699fc20cb3e45133f3d4a7d96ae5a4fbf2ea
duduumendez2 <169172180+duduumendez2@users.noreply.github.com>
2024-09-28 10:14:33 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 global.audio= "" ;  
 global.video= "" ;
 global.port =process.env.PORT
-global.appUrl=process.env.APP_URL || ""                       // put your app url here 🫵,
+global.appUrl=process.env.APP_URL || ""                       // put your app url herey 🫵,
 global.email ="saimsamsun789@gmail.com"
 global.location="Lahore,Pakistan."
 
